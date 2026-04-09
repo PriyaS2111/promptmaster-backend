988f8f2b07f34013939a79f93a7509c5b0a0695e
PriyaS2111 <priyashetty413@gmail.com>
2026-04-09 20:53:19 +0530

    Final1  CORS fix

--- app/main.py
+++ app/main.py
@@ -8,9 +8,7 @@ app = FastAPI(title="PromptMaster AI")
 app.add_middleware(
     CORSMiddleware,
     #allow_origins=["*"],   # allow all (for now)
-    allow_origins=[
-    "http://127.0.0.1:5500",
-    "http://localhost:5500"],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
