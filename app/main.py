1d91dd19b246393bd5eca0e850a076e875fd4528
PriyaS2111 <priyashetty413@gmail.com>
2026-04-09 20:48:50 +0530

    Final CORS fix

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     allow_origins=[
     "http://127.0.0.1:5500",
     "http://localhost:5500"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
